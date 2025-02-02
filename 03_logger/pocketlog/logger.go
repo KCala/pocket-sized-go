5cd64ac35ccc91e4903219fb2232ea4c7214a548
kcala <kamil.cala@gmail.com>
2025-02-02 23:47:36 +0100

    finish the logging chapter

--- 03_logger/pocketlog/logger.go
+++ 03_logger/pocketlog/logger.go
@@ -1,28 +1,68 @@
 package pocketlog
 
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
 // Logger is used to log information
 type Logger struct {
 	threshold Level
+	output    io.Writer
 }
 
-// New returns you a logger, ready to log at the required threshold
-func New(threshold Level) *Logger {
-	return &Logger{
+// New returns you a logger, ready to log at the required threshold.
+// Give it a list of configuration functions to tune it at your will.
+// The default output is Stdout.
+func New(threshold Level, opts ...Option) *Logger {
+	logger := &Logger{
 		threshold: threshold,
+		output:    os.Stdout,
 	}
+
+	for _, configFunc := range opts {
+		configFunc(logger)
+	}
+
+	return logger
 }
 
-// Debugf formats and prints a message if the log level is debug or higher
+// Debugf formats and prints a message if the log level is debug or higher.
 func (l *Logger) Debugf(format string, args ...any) {
-	//TODO implement
+	if l.output == nil {
+		l.output = os.Stdout
+	}
+	if l.threshold > LevelDebug {
+		return
+	}
+	l.logf(format, args...)
 }
 
-// Infof formats and prints a message if the log level is info or higher
+// Infof formats and prints a message if the log level is info or higher.
 func (l *Logger) Infof(format string, args ...any) {
-	// TODO implement
+	if l.output == nil {
+		l.output = os.Stdout
+	}
+	if l.threshold > LevelInfo {
+		return
+	}
+	l.logf(format, args...)
 }
 
-// Errorf formats and prints a message if the log level is error or higher
+// Errorf formats and prints a message if the log level is error or higher.
 func (l *Logger) Errorf(format string, args ...any) {
-	//TODO implement
+	if l.output == nil {
+		l.output = os.Stdout
+	}
+	if l.threshold > LevelError {
+		return
+	}
+	l.logf(format, args...)
+}
+
+// logf prints the message to the output.
+// Add decorations here, if any.
+func (l *Logger) logf(format string, args ...any) {
+	_, _ = fmt.Fprintf(l.output, format+"\n", args...)
 }
